fc033a19ec9f39ccf70ad68a9c335039c4c89e77
Christophe Hamerling <chamerling@linagora.com>
2017-06-14 15:21:47 +0200

    Add login-success.service.js

---
+++ src/overrides/login-success.service.js
@@ -0,0 +1,15 @@
+(function() {
+  'use strict';
+
+  const {ipcRenderer} = require('electron');
+
+  angular.module('esn.login')
+    .factory('loginSuccessService', loginSuccessService);
+    
+    function loginSuccessService($log) {
+      return function() {
+        $log.debug('Sending login success to main process');
+        ipcRenderer.send('op:login:success');
+      };
+    }
+})();

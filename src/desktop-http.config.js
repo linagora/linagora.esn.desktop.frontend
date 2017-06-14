32d0386853c5ff73fea72aa466939f93564df6b8
Christophe Hamerling <chamerling@linagora.com>
2017-06-14 19:29:37 +0200

    Use the privider

--- src/desktop-http.config.js
+++ src/desktop-http.config.js
@@ -4,7 +4,6 @@
   angular.module('esn.http').config(configBlock);
   
   function configBlock(httpConfigurerProvider) {
-    console.log('POUET')
     httpConfigurerProvider.setBaseUrl('http://localhost:8080');
   }
 })();
